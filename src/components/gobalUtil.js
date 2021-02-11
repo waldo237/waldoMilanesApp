71b8973de17cc54159243132fc7b2bd4711eed81
waldo237 <waldo23792@gmail.com>
2021-02-10 23:20:57 -0400

    d

--- src/components/gobalUtil.js
+++ src/components/gobalUtil.js
@@ -6,7 +6,7 @@ const uuidv4 = () => {
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
   }
-}
+} 
 
 const ColorFactory = (() => {
   function RandomColor() {
